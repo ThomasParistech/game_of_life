de501bf070c688087215916b60e7a10f126f3ae4
ThomasR <trouch@prophesee.ai>
2021-03-23 22:34:09 +0100

    add living cells

--- main.cpp
+++ main.cpp
@@ -13,7 +13,19 @@ class CellsGrid
 {
 public:
 
+    void add_living_cell(int i, int j)
+    {
+        living_cells_[i].insert(j);
+    }
+
+    void clear()
+    {
+        living_cells_.clear();
+    }
+
+
 private:
+    std::map<int, std::set<int>> living_cells_;             // Cell at (i,j) exists <=> living_cells_[i][j] exists
 };
 
 int main(int argc, char **argv)
